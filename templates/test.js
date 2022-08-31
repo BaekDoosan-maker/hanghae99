34d524cb599be7478359f8012fa662acd87f6c9a
Doosan <57098232+BaekDoosan-maker@users.noreply.github.com>
2022-09-01 05:22:54 +0900

    얼럿문구변경

--- templates/test.js
+++ templates/test.js
@@ -3,9 +3,9 @@ function check(form) {
        if(form.userpassword.value=="1234") {
              window.open('target.html')
        } else {
-           alert('Error Password');
+           alert('비밀번호가 유효하지 않습니다.');
        }
 } else {
-    alert('Error UserID'
+    alert('존재하지 않는 아이디 입니다.'
     )}
 }
